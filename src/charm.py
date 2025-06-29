2a1853acb1363249128faf6403636c1e9bc4a6db
MJ Ponsonby <mj.ponsonby@canonical.com>
2025-06-29 21:46:46 +0100

    Microcluster token distributor library
    This is a refactor for the microcluster token distributor to instead
    provide a library for charms requiring and providing this relation.
    
    This should allow easy usage for microcluster charms such as microceph
    instead of just the current microovn support.
    
    Signed-off-by: MJ Ponsonby <mj.ponsonby@canonical.com>

--- src/charm.py
+++ src/charm.py
@@ -23,18 +23,11 @@ tokens across a service using the microcluster clustering APIs.
 import logging
 
 import ops
+from charms.microcluster_token_distributor.v0.token_distributor import TokenDistributorProvides
 
 logger = logging.getLogger(__name__)
 CONTROL_RELATION = "microcluster-cluster"
 MIRROR_PREFIX = "mirror-"
-
-
-def mirror_id(hostname):
-    """Return the mirror id for the specified hostname.
-
-    :return: the mirror id for the specified hostname
-    """
-    return "{0}{1}".format(MIRROR_PREFIX, hostname)
 
 
 class TokenDistributor(ops.CharmBase):
@@ -43,40 +36,12 @@ class TokenDistributor(ops.CharmBase):
     def __init__(self, framework: ops.Framework):
         super().__init__(framework)
         framework.observe(self.on.start, self._on_start)
-        framework.observe(self.on.leader_elected, self._on_leader_elected)
-        framework.observe(self.on[CONTROL_RELATION].relation_changed, self._on_peers_changed)
-
-    def _handle_mirror(self, relation):
-        relation_data = relation.data
-        relation_data[self.unit]["mirror"] = "up"
-        for unit in relation.units:
-            if relation_data[unit].get("mirror") == "up":
-                # add all tokens in the other side of the mirror to this side
-                for k, v in relation_data[unit].items():
-                    if MIRROR_PREFIX in k:
-                        relation_data[self.unit][k] = v
-
-            if "hostname" not in relation_data[unit]:
-                continue
-            mirror_key = mirror_id(relation_data[unit]["hostname"])
-            if mirror_key not in relation_data[self.unit]:
-                logger.info("added {0} to mirror".format(mirror_key))
-                relation_data[self.unit][mirror_key] = "empty"
+        self.token_distributor = TokenDistributorProvides(
+            charm=self, relation_name=CONTROL_RELATION
+        )
 
     def _on_start(self, _: ops.StartEvent):
         self.unit.status = ops.ActiveStatus()
-
-    def _on_peers_changed(self, event: ops.RelationChangedEvent):
-        if self.unit.is_leader():
-            self._handle_mirror(event.relation)
-
-    def _on_leader_elected(self, _: ops.RelationChangedEvent):
-        if relation := self.model.get_relation(CONTROL_RELATION):
-            if self.unit.is_leader():
-                relation.data[self.unit]["mirror"] = "up"
-                self._handle_mirror(relation)
-            elif relation.data[self.unit].get("mirror"):
-                relation.data[self.unit]["mirror"] = "down"
 
 
 if __name__ == "__main__":
