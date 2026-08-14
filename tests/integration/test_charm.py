76af5e2a808380297705e64f410e50db74b6670c
MJ Ponsonby <mj.ponsonby@canonical.com>
2026-08-14 14:20:23 +0200

    Adds cross-app cluster test.
    Adds test that sets up two microovn apps and then ensures that they
    become part of the same cluster as expected. This is important for
    ensuring arm64 and amd64 microovn clusters can be friends and talk to eachother.
    
    They are modeled after the scourge sisters here to avoid having to
    handle amd64 and arm64 runners in a single test, though we should look
    at doing this at some point in the microovn repo.
    
    Signed-off-by: MJ Ponsonby <mj.ponsonby@canonical.com>

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -14,3 +14,27 @@ def test_build_and_deploy(juju: jubilant.Juju, charm_path: Path, app_name: str):
     """Deploy the charm and wait for active/idle status."""
     juju.deploy(charm_path, app=app_name)
     juju.wait(jubilant.all_active)
+
+
+def test_token_distributor_multiple_microovn(juju: jubilant.Juju, charm_path: Path, app_name: str):
+    juju.deploy(charm_path, app=app_name)
+    microovns = ["microovn-terezi", "microovn-vriska"]
+
+    for microovn in microovns:
+        juju.deploy("microovn", channel="latest/edge", app=microovn)
+        juju.integrate(microovn, app_name)
+
+    juju.wait(jubilant.all_active, timeout=600)
+    juju.wait(jubilant.all_agents_idle, timeout=600)
+
+    cluster_output = juju.exec("microovn cluster list --format csv", unit=f"{microovns[0]}/0")
+    assert len(cluster_output.stdout.split("\n")) == 2
+
+    outputs = []
+    i = 0
+    for microovn in microovns:
+        cluster_output = juju.exec("microovn cluster list --format csv", unit=f"{microovn}/0")
+        outputs[i] = "\n".join(sorted(cluster_output.stdout.split("\n")))
+        i += 1
+
+    assert outputs[0] == outputs[1]
